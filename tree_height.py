1ce25da5006a2698d60c1e96dc208ec8ce05bc7c
221RDB024 <125682199+BR4INFREEZE@users.noreply.github.com>
2023-03-06 00:08:55 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -8,53 +8,48 @@ c = 0
 
 def aray(size, numbers):
 
-    splitted = numbers.split()
-    arr1 = np.zeros(int(size))
-    i, k, start = 0, 1, 0
-    for a in range(arr1.size):
-        arr1[i] = int(splitted[i])
-        if arr1[i] == -1:
-            start = i
-        i += 1
-    check(k, start, arr1)
-
-
-def check(k, start, arr1):
-
     global c
-    for i in range(arr1.size):
-        if arr1[i] == start:
-            move = i
-            k += 1
-            if c < k:
-                c = k
-            k = check(k, move, arr1)
-    k -= 1
-    return k
+    arr1 = np.zeros(size)
+    for a in range(size):
+        k = check(a, numbers, arr1)
+        if c < k:
+            c = k
+
+
+def check(a, numbers, arr1):
+
+    if arr1[a] != 0:
+        return arr1[a]
+    elif numbers[a] == -1:
+        arr1[a] = 1
+    else:
+        arr1[a] = check(numbers[a], numbers, arr1) + 1
+    return arr1[a]
+
 
 def main():
-
+    
     first_input = input()
     if first_input.__contains__('I'):
-        second_input = input()
-        third_input = input()
+        second_input = int(input())
+        third_input = list(map(int, input().strip().split()))
         aray(second_input, third_input)
     elif first_input.__contains__('F'):
         file_name = input()
-        if os.path.exists(file_name):
-            file = os.path.join(os.getcwd(), 'test', file_name)
-            with open(file, 'r') as f:
-                second_input = f.readline()
-                third_input = str(f.readline())
-                aray(second_input, third_input)
-        else:
+        if file_name.__contains__('a'):
             print("INPUT-OUTPUT ERROR")
             return
+        file = os.path.join(os.getcwd(), 'test', file_name)
+        with open(file) as f:
+            second_input = int(f.readline())
+            third_input = list(map(int, f.readline().split()))
+            aray(second_input, third_input)
     else:
         print("INPUT-OUTPUT ERROR")
         return
-    print(c)
+    print(int(c))
 
 
-if __name__ == "__main__":
-    main()
+sys.setrecursionlimit(10**7)
+threading.stack_size(2**27)
+threading.Thread(target=main).start()
