1b6a979051cfb4b0887b396731991713467d9a25
221RDB024 <125682199+BR4INFREEZE@users.noreply.github.com>
2023-03-05 23:21:33 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -7,24 +7,20 @@ c = 0
 
 
 def aray(size, numbers):
-
-    splitted = numbers.split()
-    arr1 = np.zeros(int(size))
-    i, k, start = 0, 1, 0
-    for a in range(arr1.size):
-        arr1[i] = int(splitted[i])
-        if arr1[i] == -1:
-            start = i
-        i += 1
+    arr1 = np.zeros(size)
+    k, start = 1, 0
+    for a in range(size):
+        arr1[a] = numbers[a]
+        if arr1[a] == -1:
+            start = a
     check(k, start, arr1)
 
 
 def check(k, start, arr1):
-
     global c
-    for i in range(arr1.size):
-        if arr1[i] == start:
-            move = i
+    for a in range(arr1.size):
+        if arr1[a] == start:
+            move = a
             k += 1
             if c < k:
                 c = k
@@ -32,11 +28,11 @@ def check(k, start, arr1):
     k -= 1
     return k
 
-def main():
 
+def main():
     first_input = input()
     if first_input.__contains__('I'):
-        second_input = input()
+        second_input = int(input())
         third_input = input()
         aray(second_input, third_input)
     elif first_input.__contains__('F'):
@@ -46,13 +42,14 @@ def main():
             return
         file = os.path.join(os.getcwd(), 'test', file_name)
         with open(file, 'r') as f:
-            second_input = f.readline()
-            third_input = str(f.readline())
+            second_input = int(f.readline())
+            third_input = list(map(int, f.readline().strip().split()))
             aray(second_input, third_input)
     else:
         print("INPUT-OUTPUT ERROR")
         return
     print(c)
+
 
 sys.setrecursionlimit(10**7)
 threading.stack_size(2**27)
