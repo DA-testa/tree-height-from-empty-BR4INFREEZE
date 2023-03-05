5a93ae7ba7472a5104191ea9a447bb13615a8714
221RDB024 <125682199+BR4INFREEZE@users.noreply.github.com>
2023-03-05 20:55:32 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -40,14 +40,12 @@ def main():
         third_input = input()
         aray(second_input, third_input)
     elif first_input.__contains__('F'):
-        trash = input()
         file_name = input()
         if os.path.exists(file_name):
-            with open(file_name) as file:
+            with open("/test" + file_name) as file:
                 second_input = file.readline()
                 third_input = str(file.readline())
                 aray(second_input, third_input)
-                return
         else:
             print("INPUT-OUTPUT ERROR")
             return
