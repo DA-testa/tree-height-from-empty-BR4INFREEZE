f19c4a6ef0d63093dc32af732f1370bfbc4a3378
221RDB024 <125682199+BR4INFREEZE@users.noreply.github.com>
2023-03-06 00:08:33 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -7,33 +7,32 @@ c = 0
 
 
 def aray(size, numbers):
-    arr1 = np.zeros(size)
-    k, start = 1, 0
-    for a in range(size):
-        arr1[a] = numbers[a]
-        if arr1[a] == -1:
-            start = a
-    check(k, start, arr1)
 
-
-def check(k, start, arr1):
     global c
-    for a in range(arr1.size):
-        if arr1[a] == start:
-            move = a
-            k += 1
-            if c < k:
-                c = k
-            k = check(k, move, arr1)
-    k -= 1
-    return k
+    arr1 = np.zeros(size)
+    for a in range(size):
+        k = check(a, numbers, arr1)
+        if c < k:
+            c = k
+
+
+def check(a, numbers, arr1):
+
+    if arr1[a] != 0:
+        return arr1[a]
+    elif numbers[a] == -1:
+        arr1[a] = 1
+    else:
+        arr1[a] = check(numbers[a], numbers, arr1) + 1
+    return arr1[a]
 
 
 def main():
+    
     first_input = input()
     if first_input.__contains__('I'):
         second_input = int(input())
-        third_input = input()
+        third_input = list(map(int, input().strip().split()))
         aray(second_input, third_input)
     elif first_input.__contains__('F'):
         file_name = input()
@@ -41,14 +40,14 @@ def main():
             print("INPUT-OUTPUT ERROR")
             return
         file = os.path.join(os.getcwd(), 'test', file_name)
-        with open(file, 'r') as f:
+        with open(file) as f:
             second_input = int(f.readline())
-            third_input = list(map(int, f.readline().strip().split()))
+            third_input = list(map(int, f.readline().split()))
             aray(second_input, third_input)
     else:
         print("INPUT-OUTPUT ERROR")
         return
-    print(c)
+    print(int(c))
 
 
 sys.setrecursionlimit(10**7)
